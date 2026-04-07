1c4eef12bcf8282f0f6b09b3ce49892700137c19
Najla Abassi <abassi.nejla96@gmail.com>
2026-04-07 13:20:20 +0200

    setup tests

--- tests/testthat/setuptests_EMMA.R
+++ tests/testthat/setuptests_EMMA.R
@@ -1,2 +1,11 @@
+suppressPackageStartupMessages(
+  library("clusterProfiler")
+)
+
+suppressPackageStartupMessages(
+  library("org.Hs.eg.db")
+)
+
 data("de_res_IFNg_vs_naive", package = "EMMA")
 data("universe", package = "EMMA")
+
--- tests/testthat/test-EMMA.R
+++ tests/testthat/test-EMMA.R
@@ -12,7 +12,9 @@ test_that("EMMA_run", {
   
   expect_true("EMMA_record" %in% names(attributes(fea_res)))
   
-  expect_type(attr(fea_res, "EMMA_record"), "list")
+  expect_true(is.list(attr(fea_res, "EMMA_record")))
+  
+  expect_true(length(attr(fea_res, "EMMA_record")) == 7)
   
   expect_error(EMMA_run(enrichGO,gene = rownames(de_res_IFNg_vs_naive),
                         keyType = "ENSEMBL",
@@ -23,4 +25,36 @@ test_that("EMMA_run", {
                         universe = universe,
                         readable = TRUE))
   
+  expect_error(EMMA_run("enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+                        keyType = 'ENSEMBL',
+                        OrgDb = org.Hs.eg.db)"))
+  
+  expect_error(EMMA_run(gene = rownames(de_res_IFNg_vs_naive),
+                        keyType = "ENSEMBL",
+                        OrgDb = org.Hs.eg.db,
+                        pAdjustMethod = "BH",
+                        pvalueCutoff = 0.05,
+                        qvalueCutoff = 0.1,
+                        universe = universe,
+                        readable = TRUE))
+  
+  expect_warning(EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+                          keyType = "ENSEMBL",
+                          OrgDb = org.Hs.eg.db,
+                          pvalueCutoff = 0.05,
+                          qvalueCutoff = 0.1,
+                          universe = universe,
+                          readable = TRUE)))
+  
+  expect_warning(EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+                          keyType = "ENSEMBL",
+                          OrgDb = org.Hs.eg.db,
+                          pvalueCutoff = 0.05,
+                          qvalueCutoff = 0.1,
+                          pAdjustMethod = "BH",
+                          readable = TRUE)))
+  
+
+  
 })
+
