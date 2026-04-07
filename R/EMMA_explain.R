2b3ea7f9bce37ff1906f22b34f35c3bd6830f5e5
Najla Abassi <abassi.nejla96@gmail.com>
2026-04-07 13:17:41 +0200

    update the functions EMMA_show, EMMA_freeze and EMMA_explain with prototype functions/placeholders/docs

--- R/EMMA_explain.R
+++ R/EMMA_explain.R
@@ -4,6 +4,9 @@
 #' to a Materials and Methods section of a paper, by summarizing the executed
 #' call, the parameters, software context, and reference databases used.
 #'
+#' @param res A functional enrichment analysis results object as returned by
+#' `EMMA_run()`. Its attributes contain `EMMA_record` of class `EMMARecord`, which
+#' contains all provenance information of the performed FEA
 EMMA_explain <- function(res){
   # code
 }
--- R/EMMA_show.R
+++ R/EMMA_show.R
@@ -6,18 +6,45 @@
 #' @param res A functional enrichment analysis results object as returned by
 #' `EMMA_run()`
 #'
-#' @returns A list of the recorded information
+#' @returns NULL
 #' @export
 #'
 #' @examples
-#' res <- EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive), universe = universe, keyType = "ENSEMBL", OrgDb = org.Hs.eg.db, ont = "BP"))
+#' data("de_res_IFNg_vs_naive", package = "EMMA")
+#' data("universe", package = "EMMA")
+#' library("clusterProfiler")
+#' res <- EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+#' universe = universe, keyType = "ENSEMBL", OrgDb = org.Hs.eg.db::org.Hs.eg.db,
+#' ont = "BP"))
 #' EMMA_show(res)
 EMMA_show <- function(res){
   if ("EMMA_record" %in% names(attributes(res))) {
     message("Found EMMA record!!")
     emma_rec <- attr(res, "EMMA_record")
-    # should think of a better way to show the records
-    str(emma_rec)
+    if (!is.list(emma_rec)) {
+      stop("'EMMA_record' must be a list!")
+    }
+    
+    if (is.list(res) && "result" %in% names(res)) {
+      
+      cat("Number of Pathways: ", NROW(res$result), "\n")
+    } else {
+      cat("Number of Pathways: ", NROW(res), "\n")
+    }
+    
+    pkg_info <- emma_rec$method
+    db_info <- emma_rec$annotation
+    
+    cat("Call: ", paste(deparse(emma_rec$call), collapse = " "), " \n")
+    cat("Package: ", paste(pkg_info$package_name , "v.",
+                           pkg_info$package_version), " \n")
+    cat("Organism : ", db_info$organism, " \n")
+    cat("Gene set library : ", paste(db_info$gene_set_db, collapse = ", "), " \n")
+    cat("Gene set library version : ",db_info$gene_set_db_version, " \n")
+    cat("\n")
+    
+  } else {
+    warning("No `EMMA_record` attribute was found")
   }
   
 }
