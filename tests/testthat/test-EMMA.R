1bb2682fe2d69e5949887479b41fe828cc961b5e
Najla Abassi <abassi.nejla96@gmail.com>
2026-04-09 15:20:10 +0200

    add more tests

--- tests/testthat/test-EMMA.R
+++ tests/testthat/test-EMMA.R
@@ -101,3 +101,20 @@ test_that("testing the record content", {
   
 })
 
+
+test_that("EMMA_show", {
+  
+  fea_res <- enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+                               keyType = "ENSEMBL",
+                               OrgDb = org.Hs.eg.db,
+                               pAdjustMethod = "BH",
+                               pvalueCutoff = 0.05,
+                               qvalueCutoff = 0.1,
+                               universe = universe,
+                               readable = TRUE)
+  
+  
+  expect_warning(EMMA_show(fea_res))
+})
+
+
