b85ee3b065765ae2efa80a3fa7ef18a36beaa757
Najla Abassi <abassi.nejla96@gmail.com>
2026-04-09 15:03:12 +0200

    adding more tests

--- tests/testthat/setuptests_EMMA.R
+++ tests/testthat/setuptests_EMMA.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages(
   library("org.Hs.eg.db")
 )
 
+
 data("de_res_IFNg_vs_naive", package = "EMMA")
 data("universe", package = "EMMA")
 
--- tests/testthat/test-EMMA.R
+++ tests/testthat/test-EMMA.R
@@ -54,7 +54,50 @@ test_that("EMMA_run", {
                           pAdjustMethod = "BH",
                           readable = TRUE)))
   
+})
 
+
+test_that("getEMMARecord", {
+  fea_res <- EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+                               keyType = "ENSEMBL",
+                               OrgDb = org.Hs.eg.db,
+                               pAdjustMethod = "BH",
+                               pvalueCutoff = 0.05,
+                               qvalueCutoff = 0.1,
+                               universe = universe,
+                               readable = TRUE))
+  
+  expect_true(is.list(getEMMARecord(fea_res)))
+  expect_length(getEMMARecord(fea_res), 7)
+  
+})
+
+
+test_that("testing the record content", {
+  
+  fea_res <- EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+                               keyType = "ENSEMBL",
+                               OrgDb = org.Hs.eg.db,
+                               pAdjustMethod = "BH",
+                               pvalueCutoff = 0.05,
+                               qvalueCutoff = 0.1,
+                               universe = universe,
+                               readable = TRUE))
+  
+  emma_rec <- attr(fea_res, "EMMA_record")
+  
+  org <- emma_rec$annotation$organism
+  
+  db <- emma_rec$annotation$gene_set_db
+  
+  expect_true(is.list(emma_rec$annotation))
+  expect_true(is.list(emma_rec$method))
+  expect_true(is.list(emma_rec$input))
+  
+  expect_identical(org, "Homo sapiens")
+  expect_identical(db, "GO")
+  
+  
   
 })
 
