c8dd29a8beb0d0b657b34f09a4eebfd715791463
Federico Marini <marinif@uni-mainz.de>
2026-04-30 16:58:31 +0200

    spacing in the examples

--- R/EMMA_add_custom_metadata.R
+++ R/EMMA_add_custom_metadata.R
@@ -1,5 +1,5 @@
 #' EMMA_add_custom_metadata
-#' 
+#'
 #' Append or replace the `extra` field in the `EMMA_record` attribute
 #' of a result object returned by `EMMA_run()`. This allows users to manually
 #' provide additional annotation or contextual information that could not be
@@ -17,28 +17,34 @@
 #' data("de_res_IFNg_vs_naive", package = "EMMA")
 #' data("universe", package = "EMMA")
 #' library("clusterProfiler")
-#' res <- EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
-#' universe = universe, keyType = "ENSEMBL", OrgDb = org.Hs.eg.db::org.Hs.eg.db,
-#' ont = "BP"))
-#' res <- EMMA_add_custom_metadata(res, extra =
-#' list(note = "The background gene set list was all expressed genes in the assay"))
+#' res <- EMMA_run(
+#'   enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+#'     universe = universe, keyType = "ENSEMBL",
+#'     OrgDb = org.Hs.eg.db::org.Hs.eg.db,
+#'     ont = "BP"
+#'   )
+#' )
+#' res <- EMMA_add_custom_metadata(
+#'   res,
+#'   extra = list(note = "The background gene set list was all expressed genes in the assay")
+#' )
 EMMA_add_custom_metadata <- function(res,
                                      extra = list()) {
-  
+
   if (!is.list(extra)) {
     stop("`extra` must be a list!")
   }
-  
+
   if (length(extra) > 0L && is.null(names(extra))) {
     stop("`extra` must be a named list!")
   }
-  
+
   emma_rec <- getEMMARecord(res = res)
-  
+
   emma_rec$extra <- extra
-  
+
   # update
   attr(res, "EMMA_record") <- emma_rec
-  
+
   return(res)
 }
--- R/EMMA_explain.R
+++ R/EMMA_explain.R
@@ -14,26 +14,30 @@
 #' data("de_res_IFNg_vs_naive", package = "EMMA")
 #' data("universe", package = "EMMA")
 #' library("clusterProfiler")
-#' res <- EMMA_run(enrichGO(gene = rownames(de_res_IFNg_vs_naive),
-#' universe = universe, keyType = "ENSEMBL", OrgDb = org.Hs.eg.db::org.Hs.eg.db,
-#' ont = "BP", pAdjustMethod = "BH"))
+#' res <- EMMA_run(
+#'   enrichGO(gene = rownames(de_res_IFNg_vs_naive),
+#'     universe = universe, keyType = "ENSEMBL",
+#'     OrgDb = org.Hs.eg.db::org.Hs.eg.db,
+#'     ont = "BP", pAdjustMethod = "BH"
+#'   )
+#' )
 #' EMMA_explain(res)
-#' 
+#'
 EMMA_explain <- function(res){
-  
+
   emma_rec <- getEMMARecord(res)
-  
+
   function_name <- emma_rec$method$function_name
   pkg_name <- emma_rec$method$package_name
   pkg_version <- emma_rec$method$package_version
   db <- emma_rec$annotation$gene_set_db
   db_version <- emma_rec$annotation$gene_set_db_version
-  
+
   args <- emma_rec$input$arguments
   arg_names <- names(args)
-  
+
   message("You can always complete your text with additional information from `getEMMARecord()`!")
-  
+
   if (emma_rec$method$wrapper) {
     text <- paste0("Functional Enrichment Analysis was performed using a wrapper function ",
                    function_name, "()")
@@ -41,33 +45,33 @@ EMMA_explain <- function(res){
     text <- paste0("Functional Enrichment Analysis was performed using the ",
                    function_name, "() function")
   }
-  
+
   # checks to avoid text with NA
   if (!is.null(pkg_name) && !is.na(pkg_name)) {
     text <- paste0(text, " from the ", pkg_name, " package")
   }
-  
+
   if (!is.null(pkg_version) && !is.na(pkg_version)) {
     text <- paste0(text, " (version ", pkg_version, ")")
   }
-  
+
   if (!is.null(db) && !all(is.na(db))) {
     text <- paste0(text, " with the ",
                   paste(db, collapse = ", "), " database")
   }
-  
+
   if (!is.null(db_version) && !all(is.na(db_version))) {
     text <- paste0(text, " (version ",
                   paste(db_version, collapse = ", "),")")
   }
-  
+
   text <- paste0(text, ".")
-  
+
   ### info abt bg genes
   bg_arg <- intersect(c("universe", "background", "custom_bg", "bg_genes"),
                       arg_names)
   fdr_arg <- intersect(c("correction_method", "pAdjustMethod"), arg_names)
-  
+
   if (length(bg_arg) == 1) {
       text <- paste0(text,
         " A custom background gene set was provided (n = ",
@@ -79,8 +83,8 @@ EMMA_explain <- function(res){
   } else {
     text <- paste0(text, " No custom background gene set was recorded.")
   }
-  
-  
+
+
   ### info abt the fdr correction
   if (length(fdr_arg) == 1) {
     text <- paste0(text,
@@ -94,6 +98,6 @@ EMMA_explain <- function(res){
       text <- paste0(text, " Multiple testing correction was not applied.")
     }
   }
-  
+
   return(text)
 }
