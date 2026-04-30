848c99dfb13684d355bb741a718f8be8f32bafbe
Federico Marini <marinif@uni-mainz.de>
2026-04-30 16:57:46 +0200

    typos fixed

--- R/EMMA_freeze.R
+++ R/EMMA_freeze.R
@@ -1,5 +1,5 @@
 #' EMMA_freeze
-#' 
+#'
 #' This function records the R environment during analysis runtime and generates
 #' a lockfile that can be used with tools such as `renv`.
 #' By default, all currently loaded namespaces are recorded.
@@ -7,40 +7,40 @@
 #' @param project Character string corresponding to the path to the project
 #' directory where the lockfile should be written. If the directory does not
 #' exist, it will be created. It defaults to the current working directory
-#' @param file Character string refering to the name of the lockfile to generate.
+#' @param file Character string referring to the name of the lockfile to generate.
 #' It defaults to "renv.lock"
 #' @param pkgs Character vector of package names to snapshot. It defaults to all
 #' currently loaded namespaces via `loadedNamespaces()`
-#' 
+#'
 #' @details
 #' This function calls `renv::snapshot()` with the specified packages.
 #' The resulting lockfile can later be restored with `renv::restore()` to
 #' recreate the same package environment.
-#' 
+#'
 #' @returns TODO json file like to create a lock.file?
-#' 
+#'
 #' @seealso \code{\link[renv]{snapshot}}, \code{\link[renv]{restore}}
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' \dontrun{
-#' # Create a lockfile in a separate directory
-#' EMMA_freeze(project = "emma_env")
+#'   # Create a lockfile in a separate directory
+#'   EMMA_freeze(project = "emma_env")
 #'
-#' # Restore later with renv
-#' renv::restore(project = "emma_env")
+#'   # Restore later with renv
+#'   renv::restore(project = "emma_env")
 #' }
 EMMA_freeze <- function(project = getwd(),
                         file = "renv.lock",
                         pkgs = loadedNamespaces()){
-  
+
   # if (!dir.exists(project)) dir.create(project, recursive = TRUE)
-  # 
+  #
   # renv::snapshot(
   #   project = project,
   #   lockfile = file,
   #   packages = pkgs
   # )
-  # 
+  #
 }
