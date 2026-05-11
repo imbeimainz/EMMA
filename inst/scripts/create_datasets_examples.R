b0718a6d6d013e2546e4f7bc47b579af0fb9de2e
Najla Abassi <abassi.nejla96@gmail.com>
2026-05-11 18:52:39 +0200

    update example data

--- inst/scripts/create_datasets_examples.R
+++ inst/scripts/create_datasets_examples.R
@@ -7,15 +7,10 @@ rownames(dds_macrophage) <- substr(rownames(dds_macrophage), 1, 15)
 dds_macrophage
 
 # DE run
-keep <- rowSums(counts(dds_macrophage) >= 10) >= 6
-dds_macrophage <- dds_macrophage[keep, ]
-dds_macrophage
-
-
 # set seed for reproducibility
-set.seed(42)
+set.seed(2711)
 # sample randomly for 2k genes
-selected_genes <- sample(rownames(dds_macrophage), 2000)
+selected_genes <- sample(rownames(dds_macrophage), 500)
 
 dds_macrophage <- dds_macrophage[selected_genes, ]
 
@@ -38,10 +33,21 @@ de_res_IFNg_vs_naive <- de_res_IFNg_vs_naive[order(de_res_IFNg_vs_naive$padj), ]
 de_res_IFNg_vs_naive <- de_res_IFNg_vs_naive[!(is.na(de_res_IFNg_vs_naive$padj)) &
                                                de_res_IFNg_vs_naive$padj <= 0.05, ]
 
-universe <- rownames(dds_macrophage)
+# define gene universe
+gene_universe <- rownames(dds_macrophage)
+
+
+# perform FEA
+library("gprofiler2")
+fea_res <- gprofiler2::gost(query = de_res_IFNg_vs_naive$SYMBOL,
+                            organism = "hsapiens",
+                            correction_method = "fdr",
+                            custom_bg = universe,
+                            sources = "GO:BP") |> EMMA_run(
+                              store_session_info = FALSE,
+                              args_form = "unevaluated")
 
 save(de_res_IFNg_vs_naive, file = "de_res_IFNg_vs_naive.RData", compress = "xz")
 save(universe, file = "universe.RData", compress = "xz")
-
-
+save(fea_res, file = "fea_res.RData", compress = "xz")
 
